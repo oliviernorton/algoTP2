c8300fbf91713ce54bb5815db1707b67d6f1e411
vince1016 <45361827+vince1016@users.noreply.github.com>
2018-11-27 12:27:59 -0500

    v1.2

--- TP2PartieA.java
+++ TP2PartieA.java
@@ -15,17 +15,17 @@ public class TP2PartieA
     int value = 0;
       for(int i = binarySequence.length - 1; i >= 0; i--){
           if(binarySequence[i] == '1'){
-              value = value + getBinValue(i);
+              value = value + (int)getBinValue(i);
           }
     }
     return value;
   }
   
-  static int getBinValue(int pos){
-    int positionValue = 1;
-    int value = 0;
+  static double getBinValue(int pos){
+    double positionValue = 31;
+    double value = 0;
     for(int i = 0; i < pos; i--){
-        value =  + positionValue;
+        value = positionValue / 2;
         
     }
     return value;
