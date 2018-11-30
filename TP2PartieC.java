5ef3d99bc75c770da2a93299e3bb1d2b18afc019
vince1016 <45361827+vince1016@users.noreply.github.com>
2018-11-30 14:27:50 -0500

    v.2.0.0.0.0

--- TP2PartieC.java
+++ TP2PartieC.java
@@ -9,17 +9,52 @@ public class TP2PartieC
         char[] rep = null;
         if(isEncrypting)
         {
-            //char[] binaryCipher = encryptXOR(msgCharArray, keyCharArray);
-            //rep = binaryCipher;
+            char[] binaryCipher = encryptXOR(msgCharArray, keyCharArray);
+            rep = binaryCipher;
         }
         else
         {
             char[] binaryCipher = msgCharArray;           
-            //rep = decryptXOR(binaryCipher, keyCharArray);
+            rep = decryptXOR(binaryCipher, keyCharArray);
         }
         String s = new String(rep);
 
         return s;
     }     
+    
+    static char[] encryptXOR(char[] charMsg, char[] charKey){
+        char[] charConvertToBinary = new char[TP2PartieB.computeHowManyCellsBinaryArrayWillTake(charMsg)];
+        charConvertToBinary = TP2PartieB.convertCharArrayToBinary(charMsg);
+        char[] reConver
+        for(int i = 0; i < charConvertToBinary.length;i++){
+            int j = 0;
+            while(j < charKey.length){
+                if((charKey[j] + charConvertToBinary[j]) == 0){
+                    charConvertToBinary[i] = '0';
+                }
+                else if((charKey[j] + charConvertToBinary[j]) == 1){
+                    charConvertToBinary[i] = '1';
+                }
+                else if((charKey[j] + charConvertToBinary[j]) <= 2){
+                    charConvertToBinary[i] = '0';
+                    charConvertToBinary[i + 1] = '1';
+                }
+                j++;
+            }
+            j = 0;
+            TP2PartieA.convertFromBinaryToInt(charConvertToBinary);
+        }
+        
+        
+        return charMsg;
+    }
+    
+   
+    
+    
+    
+    static char[] decryptXOR(char[] charMsg, char[] charKey){
+        return charMsg;
+    }
 }
-               
+             
--- TP2PartieCTest.java
+++ TP2PartieCTest.java
@@ -8,5 +8,14 @@ import org.junit.Test;
 
 public class TP2PartieCTest
 {
-  
+  public void WHEN__THEN_Return(){
+      //ARRENGE
+      final char[] CHAR_MSG = {'A','L','L','O'};
+      final char[] CHAR_KEY = {'c','l'};
+      //ACT
+      char[] result = TP2PartieC.encryptXOR(CHAR_MSG,CHAR_KEY);
+      //ASSERT
+      final char[] EXPECTED_ARRAY = {'1','0','1','0','0','1','0','0','1','0','1','1','1','0','0','0','1','0','1','0','0','0','1','1','1','0','1','1','1','0','1','1'};
+      assertArrayEquals(EXPECTED_ARRAY,result);
+    }
 }
